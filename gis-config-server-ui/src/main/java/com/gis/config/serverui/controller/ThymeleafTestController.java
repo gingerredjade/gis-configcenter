37cdcdf10f91dc3bf5b88ae8006cb9de9d0bfcb9
jianghongyu <jianghy@gis.nci>
2018-08-28 16:01:54 +0800

    配置中心服务器端Web UI

---
+++ gis-config-server-ui/src/main/java/com/gis/config/serverui/controller/ThymeleafTestController.java
@@ -0,0 +1,28 @@
+package com.gis.config.serverui.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * ThymeleafTestController
+ *
+ * @since 1.0.0 2018年08月18日
+ * @author <a href="https://gisnci.com">Hongyu Jiang</a>
+ */
+//@Controller
+public class ThymeleafTestController {
+
+    /**
+     * 视图
+     * @param model
+     * @return
+     */
+    @RequestMapping("/thymeleaf/model")
+    public String page3(Model model){
+        model.addAttribute("name", "world");
+
+        // 跳转至resources/templates/的test.html
+        return "test/themeleaftest";
+    }
+}
