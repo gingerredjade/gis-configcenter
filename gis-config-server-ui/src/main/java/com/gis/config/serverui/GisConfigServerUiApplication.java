67ab674a2d12e1701e7f08f448de219bb6789296
jianghongyu <jianghy@gis.nci>
2018-08-29 17:12:03 +0800

    配置中心服务器端WebUI启动类更新

--- gis-config-server-ui/src/main/java/com/gis/config/serverui/GisConfigServerUiApplication.java
+++ gis-config-server-ui/src/main/java/com/gis/config/serverui/GisConfigServerUiApplication.java
@@ -1,12 +1,30 @@
 package com.gis.config.serverui;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * 应用启动入口类
+ * 	设置扫描了所有包
+ * 	支持定时任务
+ *
+ * 	@EnableDiscoveryClient表明它是服务注册组件的客户端（对ZK、Eureka适用）
+ * 	@EnableEurekaClient只对Eureka适用
+ *
+ * @since 1.0.0 2018年08月18日
+ * @author <a href="https://gisnci.com">Hongyu Jiang</a>
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GisConfigServerUiApplication {
+
+    private  static Logger logger = LoggerFactory.getLogger(GisConfigServerUiApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(GisConfigServerUiApplication.class, args);
+        logger.info("[GisConfigServerUi微服务] 已启动");
 	}
 }
